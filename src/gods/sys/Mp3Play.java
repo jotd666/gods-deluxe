f11f3ddb75d06ad84e85fd8736f754dbc96a9e6d
mbugni <massi.ergosum@gmail.com>
2023-10-04 14:11:49 +0200

    Fix level music path

--- src/gods/sys/Mp3Play.java
+++ src/gods/sys/Mp3Play.java
@@ -7,78 +7,73 @@ import java.io.FileInputStream;
 
 public class Mp3Play implements Runnable
 {
-	private static Mp3Play instance()
+	private static final Mp3Play m_instance = new Mp3Play();
+	
+	private String m_mp3_name;
+	private Player m_player;
+	private Thread m_play_thread;
+	private boolean m_repeat;
+	
+	public static boolean is_playing()
 	{
-		if (m_instance == null)
+		return m_instance.is_music_playing();
+	}
+
+	public static void play(String mp3_name, boolean repeat)
+	{
+		stop();
+		m_instance.m_mp3_name = mp3_name;
+		m_instance.play_music(repeat);
+	}
+
+	public static void play(String mp3_name)
+	{
+		play(mp3_name,true);
+	}
+
+	public static void replay()
+	{
+		if (m_instance.m_mp3_name != null && m_instance.m_repeat)
 		{
-			m_instance = new Mp3Play();
+			stop();
+			m_instance.play_music(true);
 		}
-		
-		return m_instance;
 	}
 	
-	private static Mp3Play m_instance = null;
+	public static void stop()
+	{
+		m_instance.stop_music();
+	}
 	
-	 private String m_mp3_name;
-	 private Player m_player;
-	 private Thread m_play_thread;
-	 private boolean m_repeat;
-	 
-	 public static boolean is_playing()
-	 {
-		 return instance().is_music_playing();
-	 }
+	private void play_music(boolean repeat)
+	{
+		m_repeat = repeat;
+		m_play_thread = new Thread(this);
+		m_play_thread.start();
+	}
 
-	 public static void play(String mp3_name, boolean repeat)
-	 {
-		 stop();
-		 instance().m_mp3_name = mp3_name;
-		 instance().play_music(repeat);
-	 }
-	 public static void play(String mp3_name)
-	 {
-		 play(mp3_name,true);
-	 }
-	 public static void replay()
-	 {
-		 if ((instance().m_mp3_name != null) && (instance().m_repeat))
-		 {
-			 stop();
-			 instance().play_music(true);
-		 }
-	 }
-	 
-	 public static void stop()
-	 {
-		 instance().stop_music();
-	 }
-	 
-	 private void play_music(boolean repeat)
-	 {
-		 m_play_thread = new Thread(this);
-		 m_repeat = repeat;
-		 m_play_thread.start();
-	 }
+	private boolean is_music_playing()
+	{
+		return m_play_thread != null && m_play_thread.isAlive();
+	}
 
-	 private boolean is_music_playing()
-	 {
-		  return m_play_thread != null && m_play_thread.isAlive();
-	 }
-	 private synchronized void stop_music()
-	 {
-		 if (m_player != null)
-		 {
-		 m_player.close();
-		 m_player = null;
-		 m_play_thread = null;
-		 }
-	 }
+	private synchronized void stop_music()
+	{
+		if (m_player != null)
+		{
+			System.out.println("Stop playing " + m_mp3_name);
+			m_player.close();
+			m_player = null;
+			m_play_thread = null;
+		}
+	}
 
 	public void run()
 	{
 		try (FileInputStream fileStream = new FileInputStream(m_mp3_name);
-			 BufferedInputStream bufferedStream = new BufferedInputStream(fileStream))
+				BufferedInputStream bufferedStream = new BufferedInputStream(fileStream))
 		{
+			System.out.println("Start playing " + m_mp3_name);
 			m_player = new Player(bufferedStream);
 			m_player.play();
 		}
